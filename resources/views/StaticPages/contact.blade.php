2b0162272a5fa46e0303b50037001dfcf960cdc3
mmalinovski <mmalinovski@gmail.com>
2017-08-30 16:42:32 +0200

    E-mail encoder

--- resources/views/StaticPages/contact.blade.php
+++ resources/views/StaticPages/contact.blade.php
@@ -5,7 +5,11 @@
 <main>
 	<section class="staticWrapper">
 		<br><br><br>
-		<p>Welcome to LiveNetworks radio! If you have questions, feedback or suggestions on the service of LiveNetworks or on cooperations, please feel free to contact us on this e-mail: <b><i>contact@livenetworks.com</i></b></p>
+		<p>Welcome to LiveNetworks radio! If you have questions, feedback or suggestions on the service of LiveNetworks or on cooperations, please feel free to contact us on this e-mail:
+			<script type="text/javascript">
+				document.write("<a href=\"mail" + "to:" + new Array("contact","livenetworks.com").join("@") + "\">" + new Array("contact","livenetworks.com").join("@") + "</" + "a>");
+			</script>
+		</p>
 		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 	</section>
 </main>
