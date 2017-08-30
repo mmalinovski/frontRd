bcbe4892b5a7b573bad4112c6a65f9a2ea26b152
mmalinovski <mmalinovski@gmail.com>
2017-08-30 16:31:37 +0200

    About and Contact updated

--- resources/views/StaticPages/about.blade.php
+++ resources/views/StaticPages/about.blade.php
@@ -4,7 +4,9 @@
 
 <main>
 	<section class="staticWrapper">
+		<br><br><br>
 		<p>LiveNetworks offers more than 30.000 radio stations and podcasts and reports a number of 4 million monthly uniqe users and 20 million app downloads worldwide. The website of LiveNetworks offers free access to radio stations and podcasts from all over the world. Discover the world of radio directly in your browser. With the LiveNetworks apps you have the entire range of radio stations on your smartphone or tablet. Listen to your favorite stations or discover new ones from around the world.</p>
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 	</section>
 </main>
 
--- resources/views/StaticPages/contact.blade.php
+++ resources/views/StaticPages/contact.blade.php
@@ -4,7 +4,9 @@
 
 <main>
 	<section class="staticWrapper">
+		<br><br><br>
 		<p>Welcome to LiveNetworks radio! If you have questions, feedback or suggestions on the service of LiveNetworks or on cooperations, please feel free to contact us on this e-mail: <b><i>contact@livenetworks.com</i></b></p>
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 	</section>
 </main>
 
