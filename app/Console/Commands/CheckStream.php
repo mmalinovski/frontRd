8b617fb6c84a5725f8eaee8a0db32d6868aa6797
mmalinovski <mmalinovski@gmail.com>
2017-12-01 15:06:55 +0100

    CheckStream

--- app/Console/Commands/CheckStream.php
+++ app/Console/Commands/CheckStream.php
@@ -9,73 +9,77 @@ use App\Models\RadioDetail;
 
 class CheckStream extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'CheckStream';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'CheckStream';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Checks the streams if valid or not';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Checks the streams if valid or not';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        $this->info("Ready for checking url!");
-        $counter = 0;
-        while($url = Station::whereNull('status')->first()) {
-            $counter ++;
-            $url->status = 'Checking';
-            $url->save();
-            echo "\n";
-            echo 'Url no.'.$counter."\n";
-            echo $url->streams[0]->listenurl."\n";
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		$this->info("Ready for checking url!");
+		$counter = 0;
+		while($url = Station::whereNull('status')->first()) {
+			$counter ++;
+			$url->status = 'Checking';
+			$url->save();
+			echo "\n";
+			echo 'Url no.'.$counter."\n";
+			echo $url->streams[0]->listenurl."\n";
 
-            $urlCheck = $url->streams[0]->listenurl;
-            $urlHeaders = @get_headers($urlCheck);
-            if($urlHeaders === false) return false;
-            foreach($urlHeaders as $header) { // parse all headers:
-                // corrects $url when 301/302 redirect(s) lead(s) to 200:
-                if(preg_match("/^Location: (http.+)$/",$header,$m)) $urlCheck=$m[1]; 
-                // grabs the last $header $code, in case of redirect(s):
-                if(preg_match("/^HTTP.+\s(\d\d\d)\s/",$header,$m)) $code=$m[1]; 
-              } // End foreach...
-              if($code==200) {
-                $url->active = '1';
-                echo 'Url uredu.';
+			$urlCheck = $url->streams[0]->listenurl;
+			$urlHeaders = @get_headers($urlCheck);
+			if($urlHeaders === false) {
+				$url->active = '0';
+				echo 'Url ne e uredu.';
+			}
+			else {
+				foreach($urlHeaders as $header) { // parse all headers:
+					// corrects $url when 301/302 redirect(s) lead(s) to 200:
+					if(preg_match("/^Location: (http.+)$/",$header,$m)) $urlCheck=$m[1]; 
+					// grabs the last $header $code, in case of redirect(s):
+					if(preg_match("/^HTTP.+\s(\d\d\d)\s/",$header,$m)) $code=$m[1]; 
+				} // End foreach...
+				print_r($code);
+				if($code==200) {
+					$url->active = '1';
+					echo 'Url uredu.';
 
-              } else {
-                $url->active = '0';
-                echo 'Url ne e uredu.';
-              }
+				} else {
+					$url->active = '0';
+					echo 'Url ne e uredu.';
+				}
 
-                $url->status = 'Done';
-                $url->save();
+			}
+			$url->status = 'Done';
+			$url->save();
+		}
 
 
-        }
-
-
-    }
+	}
 }
 
 
