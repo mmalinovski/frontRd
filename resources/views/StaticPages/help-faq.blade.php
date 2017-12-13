df6106d1d621199111b467f86923d2cf1fc7e2f5
mmalinovski <mmalinovski@gmail.com>
2017-12-13 08:15:48 +0100

    faq2

--- resources/views/StaticPages/help-faq.blade.php
+++ resources/views/StaticPages/help-faq.blade.php
@@ -10,79 +10,79 @@
 
 			<h2>Listeners:</h2>
 
-			<p>Q: What is Nextuner?</p>
+			<p>Q: What is Nextuner?</p><br>
 
 			<p>A: Nextuner is an intenret radio and podcast directory with over 44,000 stations listed worldwide. The goal is 100,000 stations.</p>
 
-			<p>Q: Will Nextuner have mobile apps?</p>
+			<p>Q: Will Nextuner have mobile apps?</p><br>
 
 			<p>A: Yes! We are currently working on website v2 and v3 as well as mobile apps in popular formats. We will also work hard to be on your car radio with future apps!</p>
 
-			<p>Q: What if a station isn't working?</p>
+			<p>Q: What if a station isn't working?</p><br>
 
 			<p>A: Please notify us of this problem here (link to form) Some stations do not broadcast 24/7 and we will do our best to update stations that have not been updated by their owners on Nextuner. You should see a "report" link and you will be given the option to tell us the station isn't working properly.</p>
 
-			<p>Q: Some stations have commercials and some do not, why?</p>
+			<p>Q: Some stations have commercials and some do not, why?</p><br>
 
 			<p>A: Yes, some stations will have commericals and some are commerical free.. That is up to each station and your choice to listen to them or not.</p>
 
-			<p>Q: Will Nextuner run commercials?</p>
+			<p>Q: Will Nextuner run commercials?</p><br>
 
 			<p>A: Nextuner MAY run ads BEFORE the stream plays. Nextner has plans to run pre-roll ads and banner ads only. We will NOT run in-stream ads or interrupt any stream in progress.</p>
 
-			<p>Q: Will Nextuner offer premium content and have Nextuner branded stations?</p>
+			<p>Q: Will Nextuner offer premium content and have Nextuner branded stations?</p><br>
 
 			<p>A: NO we will not! We will not compete with any station in our network with Nextuner branded stations or offer non commerical versions for a monthly fee. Use of Nextuner services is free and paid for by pre-roll and banner advertisng.</p>
 
-			<p>Q: What are editors picks?</p>
+			<p>Q: What are editors picks?</p><br>
 
 			<p>A: Nextuner will choose the best programmed stations on a weekly basis and list them for 7 days on the Nextuner home page. There are multiple criterias considered and all of it is based on content and how it's delivered.</p>
 
-			<p>Q: What about hate speech?</p>
+			<p>Q: What about hate speech?</p><br>
 
 			<p>A: Nextuner believes in all speech whether someone likes it or not. If a station bothers you please do not listen to it! Propaganda is another issue and is dealt with on a case bycase basis.</p>
 
 			<h2>Broadcasters:</h2>
 
-			<p>Q: Is there a fee to be listed on Nextuner?
+			<p>Q: Is there a fee to be listed on Nextuner?</p><br>
 
-			A: No and there never will be!
+			<p>A: No and there never will be!</p>
 
-			Q: Will there be a fee to be an editors pick?
+			<p>Q: Will there be a fee to be an editors pick?</p><br>
 
-			A: No and there never will be. Criteria is subjective and based on how content is presented, professionalsim and creativity along with other factors.    
+			<p>A: No and there never will be. Criteria is subjective and based on how content is presented, professionalsim and creativity along with other factors.</p>  
 
-			Q: Can my station run commericals?
+			<p>Q: Can my station run commericals?</p><br>
 
-			A: Of course you can but we cannot do that for you. Nextuner will not interrupt any stream in progress with injected ads.. We can refer you to companies who offer this service. Please use the contact us form.
+			<p>A: Of course you can but we cannot do that for you. Nextuner will not interrupt any stream in progress with injected ads.. We can refer you to companies who offer this service. Please use the contact us form.</p>
 
-			Q: Will Nextuner have branded stations and commercial free stations?
+			<p>Q: Will Nextuner have branded stations and commercial free stations?</p><br>
 
-			A: NO we will not! Nextuner will not compete with you with Nextuner branded stations.
+			<p>A: NO we will not! Nextuner will not compete with you with Nextuner branded stations.</p>
 
-			Q: Will you run advertising?
+			<p>Q: Will you run advertising?</p><br>
 
-			A: YES and only pre-roll (before the stream starts along with banner ads) That's it.
+			<p>A: YES and only pre-roll (before the stream starts along with banner ads) That's it.</p>
 
-			Q: Do you share revenue?
+			<p>Q: Do you share revenue?</p><br>
 
-			A: No we do not. The reason? We will use revenue for future web and app development which is constant and to fund a legal team (however possible) to represent US based stations fighting for affordable royalty fees. The independent station was NOT represented last meeting. Our goal will be ZERO royalty fees.
+			<p>A: No we do not. The reason? We will use revenue for future web and app development which is constant and to fund a legal team (however possible) to represent US based stations fighting for affordable royalty fees. The independent station was NOT represented last meeting. Our goal will be ZERO royalty fees.</p>
 
-			Q: Will broadcasters be able to make changes to their station?
+			<p>Q: Will broadcasters be able to make changes to their station?</p><br>
 
-			A: YES! There is a back office for station owners where you can make changes and updates to your station on the fly..
+			<p>A: YES! There is a back office for station owners where you can make changes and updates to your station on the fly..</p>
 
-			Q: What if my station is already listed? Will I be able to claim it?
+			<p>Q: What if my station is already listed? Will I be able to claim it?</p><br>
 
-			A: YES, after you prove ownership! There is a claim your station form here: (Link)
+			<p>A: YES, after you prove ownership! There is a claim your station form here: (Link)</p>
 
-			Q: Will I be able to use my nextuner page as my station home page?
+			<p>Q: Will I be able to use my nextuner page as my station home page?</p><br>
 
-			A: Yes, and we encourage it! All stations will have a subdomain listing similar to: http://station.nextuner.com
+			<p>A: Yes, and we encourage it! All stations will have a subdomain listing similar to: http://station.nextuner.com</p>
 
-			Q: Will there be a fee for this?
+			<p>Q: Will there be a fee for this?</p><br>
 
-			A: No and we would appreciate you sharing Nextuner and supporting nextuner.com as THE directory that supports independent internet broadcasters worldwide.</p>
+			<p>A: No and we would appreciate you sharing Nextuner and supporting nextuner.com as THE directory that supports independent internet broadcasters worldwide.</p>
 
 
 			
