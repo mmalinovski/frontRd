23b8a7b0d53a56cabf5fb961ccea240c83fd3ef2
mmalinovski <mmalinovski@gmail.com>
2017-12-13 08:12:39 +0100

    faq1

--- resources/views/StaticPages/help-faq.blade.php
+++ resources/views/StaticPages/help-faq.blade.php
@@ -10,37 +10,37 @@
 
 			<h2>Listeners:</h2>
 
-			<p>Q: What is Nextuner?
+			<p>Q: What is Nextuner?</p>
 
-			A: Nextuner is an intenret radio and podcast directory with over 44,000 stations listed worldwide. The goal is 100,000 stations.
+			<p>A: Nextuner is an intenret radio and podcast directory with over 44,000 stations listed worldwide. The goal is 100,000 stations.</p>
 
-			Q: Will Nextuner have mobile apps?
+			<p>Q: Will Nextuner have mobile apps?</p>
 
-			A: Yes! We are currently working on website v2 and v3 as well as mobile apps in popular formats. We will also work hard to be on your car radio with future apps!
+			<p>A: Yes! We are currently working on website v2 and v3 as well as mobile apps in popular formats. We will also work hard to be on your car radio with future apps!</p>
 
-			Q: What if a station isn't working?
+			<p>Q: What if a station isn't working?</p>
 
-			A: Please notify us of this problem here (link to form) Some stations do not broadcast 24/7 and we will do our best to update stations that have not been updated by their owners on Nextuner. You should see a "report" link and you will be given the option to tell us the station isn't working properly.
+			<p>A: Please notify us of this problem here (link to form) Some stations do not broadcast 24/7 and we will do our best to update stations that have not been updated by their owners on Nextuner. You should see a "report" link and you will be given the option to tell us the station isn't working properly.</p>
 
-			Q: Some stations have commercials and some do not, why?
+			<p>Q: Some stations have commercials and some do not, why?</p>
 
-			A: Yes, some stations will have commericals and some are commerical free.. That is up to each station and your choice to listen to them or not.  
+			<p>A: Yes, some stations will have commericals and some are commerical free.. That is up to each station and your choice to listen to them or not.</p>
 
-			Q: Will Nextuner run commercials?
+			<p>Q: Will Nextuner run commercials?</p>
 
-			A: Nextuner MAY run ads BEFORE the stream plays. Nextner has plans to run pre-roll ads and banner ads only. We will NOT run in-stream ads or interrupt any stream in progress.
+			<p>A: Nextuner MAY run ads BEFORE the stream plays. Nextner has plans to run pre-roll ads and banner ads only. We will NOT run in-stream ads or interrupt any stream in progress.</p>
 
-			Q: Will Nextuner offer premium content and have Nextuner branded stations?
+			<p>Q: Will Nextuner offer premium content and have Nextuner branded stations?</p>
 
-			A: NO we will not! We will not compete with any station in our network with Nextuner branded stations or offer non commerical versions for a monthly fee. Use of Nextuner services is free and paid for by pre-roll and banner advertisng.
+			<p>A: NO we will not! We will not compete with any station in our network with Nextuner branded stations or offer non commerical versions for a monthly fee. Use of Nextuner services is free and paid for by pre-roll and banner advertisng.</p>
 
-			Q: What are editors picks?
+			<p>Q: What are editors picks?</p>
 
-			A: Nextuner will choose the best programmed stations on a weekly basis and list them for 7 days on the Nextuner home page. There are multiple criterias considered and all of it is based on content and how it's delivered.
+			<p>A: Nextuner will choose the best programmed stations on a weekly basis and list them for 7 days on the Nextuner home page. There are multiple criterias considered and all of it is based on content and how it's delivered.</p>
 
-			Q: What about hate speech?
+			<p>Q: What about hate speech?</p>
 
-			A: Nextuner believes in all speech whether someone likes it or not. If a station bothers you please do not listen to it! Propaganda is another issue and is dealt with on a case bycase basis.</p>
+			<p>A: Nextuner believes in all speech whether someone likes it or not. If a station bothers you please do not listen to it! Propaganda is another issue and is dealt with on a case bycase basis.</p>
 
 			<h2>Broadcasters:</h2>
 
